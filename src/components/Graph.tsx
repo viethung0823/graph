81e2c88a2a9dff5c2216780e40117b4e727f5f4e
saberzero1 <github@emilebangma.com>
2026-02-07 06:13:00 +0100

    fix: use any[] for externalPlugins type

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -17,7 +17,7 @@ type GlobalConfiguration = {
 type QuartzConfig = {
   configuration: GlobalConfiguration;
   plugins: any;
-  externalPlugins?: string[];
+  externalPlugins?: any[];
 };
 
 type QuartzPluginData = {
